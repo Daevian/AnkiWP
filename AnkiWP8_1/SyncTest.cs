6b711ae3ca95714b5b9b72ccdb97ae7242681a09
daevian <daevian@gmail.com>
2015-02-11 23:04:35 +0000

    does a full sync stores database to disk

--- AnkiWP8_1/SyncTest.cs
+++ AnkiWP8_1/SyncTest.cs
@@ -21,8 +21,20 @@ namespace AnkiWP8_1
 {
     public class SyncTest
     {
+        public static string SYNC_VER = "8";
+        public static string version = "2.0.31";
+
+        public static Col m_col = new Col();
         public static string m_hKey = string.Empty;
         public static string m_sKey = string.Empty;
+        public static ulong m_rmod;     // mod?
+        public static ulong m_lmod;     // mod?
+        public static ulong m_maxUsn;   // usn?
+        public static ulong m_minUsn;   // usn?
+        public static ulong m_mediaUsn; // musn?
+        public static string m_syncMsg; // msg?
+        public static string m_uname;   // uname?
+        public static bool m_lnewer;    // newer?
 
         public static async void ApplicationResuming()
         {
@@ -70,6 +82,8 @@ namespace AnkiWP8_1
 
         public static async void ConnectionTest()
         {
+            m_col.Path = "collection.anki2";
+
             // skey
             {
                 var random = new Random();
@@ -111,12 +125,11 @@ namespace AnkiWP8_1
                 try
                 {
                     Dictionary<string, object> postVars = new Dictionary<string, object>();
-                    string content = await Request("hostKey", writer.DetachBuffer(), postVars);
+                    var content = await Request("hostKey", writer.DetachBuffer(), postVars);
+                    string contentString = await content.ReadAsStringAsync();
                     Windows.Data.Json.JsonObject a;
-                    bool result = Windows.Data.Json.JsonObject.TryParse(content, out a);
-                    Windows.Data.Json.IJsonValue jValue;
-                    result = a.TryGetValue("key", out jValue);
-                    m_hKey = jValue.GetString();
+                    bool result = Windows.Data.Json.JsonObject.TryParse(contentString, out a);
+                    m_hKey = a.GetNamedString("key", string.Empty);
                 }
                 catch (Exception e)
                 {
@@ -125,17 +138,142 @@ namespace AnkiWP8_1
                 }
             }
 
-            TestSync();
+            SyncResult syncResult = await Sync();
+            switch (syncResult)
+            {
+                case SyncResult.Success:
+                    break;
+                case SyncResult.NoChanges:
+                    break;
+                case SyncResult.ServerAbort:
+                    Debug.WriteLine("Server Abort: " + m_syncMsg);
+                    break;
+                case SyncResult.ClockOff:
+                    break;
+                case SyncResult.FullSync:
+                    FullSync();
+                    break;
+                case SyncResult.BadAuth:
+                    break;
+                case SyncResult.BasicCheckFailed:
+                    break;
+                case SyncResult.SanityCheckFailed:
+                    break;
+                case SyncResult.CheckFailed:
+                    break;
+                default:
+                    Debug.Assert(false);
+                    break;
+            }
+
+            // sync media
+
         }
 
-        public static async void TestSync()
+        public static async void FullSync()
         {
+            FullSyncChoice choice = FullSyncChoice.Cancel;
+            if (m_col.IsEmpty)
+            {
+                choice = FullSyncChoice.Download;
+            }
+            else
+            {
+                // ask
+            }
+
+            switch (choice)
+            {
+                case FullSyncChoice.Download:
+                    Download();
+                    break;
+                case FullSyncChoice.Upload:
+                    bool success = await Upload();
+                    if (!success)
+                    {
+                        // bad upload
+                    }
+                    break;
+                case FullSyncChoice.Cancel:
+                    return;
+                default:
+                    Debug.Assert(false);
+                    break;
+            }
+
+            m_col.Reopen();
+            // self._syncMedia()
+
+        }
+
+        public static async void Download()
+        {
+            m_col.Close();
+
+            string v = string.Format("ankidesktop,{0},{1}", version, "win:8");
+
+            Dictionary<string, object> postVars = new Dictionary<string, object>();
+            postVars.Add("k", m_hKey);
+            postVars.Add("v", v);
+
+            var content = await Request("download", postVars: postVars);
+            string contentString = await content.ReadAsStringAsync();
+            if (contentString.Equals("upgradeRequired"))
+            {
+                return;
+            }
+
+            string tempPath = m_col.Path + ".tmp";
+
+            var localFolder = ApplicationData.Current.LocalFolder;
+            StorageFile file = await localFolder.CreateFileAsync(tempPath, CreationCollisionOption.ReplaceExisting);
+            using (var transaction = await file.OpenTransactedWriteAsync())
+            {
+                await content.WriteToStreamAsync(transaction.Stream);
+                await transaction.CommitAsync();
+            }
+            
+            /*
+            d = DB(tpath)
+            assert d.scalar("pragma integrity_check") == "ok"
+            d.close()
+             */
+
+            await file.RenameAsync(m_col.Path, NameCollisionOption.ReplaceExisting);
+        }
+
+        public static async Task<bool> Upload()
+        {
+            return false;
+        }
+
+        public enum FullSyncChoice
+        {
+            Download,
+            Upload,
+            Cancel,
+        }
+
+        public enum SyncResult
+        {
+            Success,
+            NoChanges,
+            ServerAbort,
+            ClockOff,
+            FullSync,
+            BadAuth,
+            BasicCheckFailed,
+            SanityCheckFailed,
+            CheckFailed,
+        };
+
+        public static async Task<SyncResult> Sync()
+        {
+            // step 1: login & metadata
             Dictionary<string, object> postVars = new Dictionary<string, object>();
             postVars.Add("k", m_hKey);
             postVars.Add("s", m_sKey);
 
-            string SYNC_VER = "8";
-            string version = "2.0.31";
             string cv = string.Format("ankidesktop,{0},{1}", version, "win:8");
 
             Windows.Data.Json.JsonObject jtest = new Windows.Data.Json.JsonObject
@@ -150,23 +288,99 @@ namespace AnkiWP8_1
 
             IBuffer buffer = writer.DetachBuffer();
 
-            string content = await Request("meta", buffer, postVars);
+            var content = await Request("meta", buffer, postVars);
+            string contentString = await content.ReadAsStringAsync();
+
             Windows.Data.Json.JsonObject a;
-            bool result = Windows.Data.Json.JsonObject.TryParse(content, out a);
-            Windows.Data.Json.IJsonValue jValue;
-            result = a.TryGetValue("key", out jValue);
-            m_hKey = jValue.GetString();
+            bool result = Windows.Data.Json.JsonObject.TryParse(contentString, out a);
+
+            ulong rscm = Convert.ToUInt64(a.GetNamedNumber("scm", 0.0));    // remote schema?
+            ulong rts = Convert.ToUInt64(a.GetNamedNumber("ts", 0.0));      // remote timestamp?
+
+            m_rmod = Convert.ToUInt64(a.GetNamedNumber("mod", 0.0));
+            m_maxUsn = Convert.ToUInt64(a.GetNamedNumber("usn", 0.0));
+            m_mediaUsn = Convert.ToUInt64(a.GetNamedNumber("musn", 0.0));
+            m_syncMsg = a.GetNamedString("msg", string.Empty);
+            m_uname = a.GetNamedString("uname", string.Empty);
+
+            bool cont = a.GetNamedBoolean("cont", false);
+            if (!cont) // continue?
+            {
+                return SyncResult.ServerAbort;
+            }
+
+            Meta meta = GetMeta();
+            m_lmod = meta.mod;
+            m_minUsn = meta.usn;
+            ulong lscm = meta.scm;
+            ulong lts = meta.ts;
+
+            if (Math.Abs((long)(rts - lts)) > 300)
+            {
+                return SyncResult.ClockOff;
+            }
+
+            if (m_lmod == m_rmod)
+            {
+                return SyncResult.NoChanges;
+            }
+            else if (lscm != rscm)
+            {
+                return SyncResult.FullSync;
+            }
+
+            m_lnewer = m_lmod > m_rmod;
+
+            // step 1.5: check collection is valid
+
+            // step 2: deletions
+
+            // ...and small objects
+
+            // step 3: stream large tables from server
+
+            // step 4: stream to server
+
+            // step 5: sanity check
+
+            // finalize
+
+
+            return SyncResult.Success;
         }
 
-        public static async Task<string> Request(string method, IBuffer fobj, Dictionary<string, object> postVars, int comp = 6, bool badAuthRaises = false)
+        public static Meta GetMeta()
         {
-            Debug.Assert(postVars != null);
+            Meta meta;
+            meta.mod = m_col.mod;
+            meta.scm = m_col.scm;
+            meta.usn = m_col.usn;
+            meta.ts = IntTime();
+            meta.musn = 0;
+            meta.msg = string.Empty;
+            meta.cont = true;
+            return meta;
+        }
 
+        public static ulong IntTime(ulong scale = 1)
+        {
+            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            ulong secondsSinceEpoch = (ulong)t.TotalSeconds;
+
+            return secondsSinceEpoch * scale;
+        }
+
+        public static async Task<IHttpContent> Request(string method, IBuffer fobj = null, Dictionary<string, object> postVars = null, int comp = 6, bool badAuthRaises = false)
+        {
             string BOUNDARY = "Anki-sync-boundary";
             string boundary = "--" + BOUNDARY;
             DataWriter writer = new DataWriter();
             
             // compression flag and session key as post vars
+            if (postVars == null)
+            {
+                postVars = new Dictionary<string, object>();
+            }
             postVars["c"] = comp > 0 ? 1 : 0;
 
             foreach (var item in postVars)
@@ -222,8 +436,7 @@ namespace AnkiWP8_1
 
             if (response.IsSuccessStatusCode)
             {
-                string resContent = await response.Content.ReadAsStringAsync();
-                return resContent;
+                return response.Content;
             }
             else
             {
@@ -240,8 +453,43 @@ namespace AnkiWP8_1
                 // - 503/504: server too busy
             }
 
-            return string.Empty;
+            return null;
         }
+    }
+
+    public class Col
+    {
+        public ulong mod;
+        public ulong scm;
+        public ulong usn;
+
+        public string Path { get; set; }
+
+        public bool IsEmpty
+        {
+            get { return true; }
+        }
+
+        public void Close()
+        {
+
+        }
+
+        public void Reopen()
+        {
+
+        }
+    }
+
+    public struct Meta
+    {
+        public ulong mod;
+        public ulong scm;
+        public ulong usn;
+        public ulong ts;
+        public ulong musn;
+        public string msg;
+        public bool cont;
     }
 
     public class HttpAnkiContent : IHttpContent
